3a7ee89ea787675db658d34efe54b345a4740d79
DanielTsuruga <daniel.tsuruga@gmail.com>
2026-08-27 12:21:50 -0300

    conclusao

--- Clima/src/main/java/com/example/Clima/service/Service.java
+++ Clima/src/main/java/com/example/Clima/service/Service.java
@@ -5,8 +5,7 @@ import org.springframework.web.client.RestTemplate;
 
 public class Service {
 
-    private static final String BASE_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m";
-
+    private static final String BASE_URL = "https://api.open-meteo.com/v1/forecast?latitude=-19.9208&longitude=-43.9378&daily=temperature_2m_min,temperature_2m_max&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,wind_direction_10m&current=weather_code&timezone=America%2FSao_Paulo";
         public String preverTempo() {
 
         RestTemplate restTemplate = new RestTemplate();
