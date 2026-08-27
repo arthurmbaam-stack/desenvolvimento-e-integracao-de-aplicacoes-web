3b6ab54c5ba63817173fc01b4010ccad5ab728b3
Gabriel Hermont <gabrielsjh2007@gmail.com>
2026-08-27 12:03:52 -0300

    wow

--- Clima/src/main/java/com/example/Clima/controller/Controller.java
+++ Clima/src/main/java/com/example/Clima/controller/Controller.java
@@ -1,5 +1,20 @@
 package com.example.Clima.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.Clima.service.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
 public class Controller {
+
+    Service service = new Service();
+
+    //https://localhost:8080/clima
+    @GetMapping("/clima")
+    public String getClima() {
+        return service.preverTempo();
+    }
+    
 
 }
--- Clima/src/main/java/com/example/Clima/service/Service.java
+++ Clima/src/main/java/com/example/Clima/service/Service.java
@@ -1,5 +1,22 @@
 package com.example.Clima.service;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 public class Service {
+
+    private static final String BASE_URL = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m";
+
+        public String preverTempo() {
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASE_URL, String.class);
+
+        if (responseEntity.getStatusCode().is2xxSuccessful()) {
+            return responseEntity.getBody();
+        } else {
+            return "Falha ao obter dados meteorológicos. Código: " + responseEntity.getStatusCode();
+        }
+    }
 
 }
